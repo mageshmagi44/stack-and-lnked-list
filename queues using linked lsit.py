7e2257e7a2bbeaa46aa4eed9c450b9e6a7a89f40
mageshmagi44 <mageshmagi44@gmail.com>
2023-11-01 12:01:12 +0530

    addedd odne

--- queues using linked lsit.py
+++ queues using linked lsit.py
@@ -4,8 +4,18 @@ class node:
     self.next=None
 
 class Queue:
-  def __init__(self):
-    self.front=None
-    self.rear=None
+    def __init__(self):
+        self.front=None
+        self.rear=None
     
+    def enqueue(self,data):
+       newnode=node(data)
+       if self.rear==None:
+          self.front=newnode=self.rear
+
+    def traverse(self):
+       curr=self.front
+       while curr:
+          print(curr.data,end=" ")
+          curr=curr.next
   
