b6a0b464bb73a6866c300853a1d53462cce188ac
mageshmagi44 <mageshmagi44@gmail.com>
2023-11-01 19:20:51 +0530

    dlinklis

---
+++ doubly linked list.py
@@ -0,0 +1,27 @@
+class node:
+    def __init__(self,data) -> None:
+        self.data=data
+        self.nref=None
+        self.pref=None
+
+class doublyll:
+    def __init__(self) -> None:
+        self.head=None
+
+    def traverse(self):
+        if self.head==None:
+            return "empty likedlist"
+        n=self.head
+        while n:
+            print(n.data,"-->",end=" ")
+            n=n.nref
+
+    def revtraverse(self):
+        if self.head==None:
+            return "empty likedlist"
+        n=self.head
+        while n.nref:
+            n=n.ref
+        while n:
+            print(n.data,"-->",end=" ")
+            n=n.pref
