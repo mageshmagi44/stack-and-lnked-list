162de6b7b72610e366929761ea25d508176d760d
mageshmagi44 <mageshmagi44@gmail.com>
2023-11-01 09:41:09 +0530

    added  pop traverse

--- stack implementaion using list.py
+++ stack implementaion using list.py
@@ -5,5 +5,29 @@ class stack:
         self.top=-1
 
 
-s=stack(3)
-s.stack
+    def push(self,value):
+        if self.top==self.size-1:
+            return "over flow"
+        else:
+            self.top+=1
+            self.stack[self.top]=value
+    def pop(self):
+        if self.top==-1:
+            return "emppty"
+        else:
+            data=self.stack[self.top]
+            self.top-=1
+            print(data)
+
+    def traverse(self):
+        for i in range(self.top+1):
+            print(self.stack[i],end=" ")
+
+
+s=stack(4)
+s.push(1)
+s.push(11)
+s.push(100)
+s.pop()
+s.pop()
+s.traverse()
