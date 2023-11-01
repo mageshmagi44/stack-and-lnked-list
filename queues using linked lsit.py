89c0092b6704f52caa7068647e2eb3716c2f5f2d
mageshmagi44 <mageshmagi44@gmail.com>
2023-11-01 12:12:57 +0530

    completed

--- queues using linked lsit.py
+++ queues using linked lsit.py
@@ -11,7 +11,29 @@ class Queue:
     def enqueue(self,data):
        newnode=node(data)
        if self.rear==None:
-          self.front=newnode=self.rear
+          self.front=self.rear=newnode
+       else:
+          self.rear.next=newnode
+          self.rear=newnode
+
+    def dequue(self):
+       if self.front==None:
+          return "queue is empty"
+       else:
+          self.front=self.front.next
+
+    def isempty(self):
+       return self.front==None
+    
+    def size(self):
+       curr=self.front
+       count=0
+       while curr:
+          count+=1
+          curr=curr.next
+       print(count)
+       
+        
 
     def traverse(self):
        curr=self.front
@@ -19,3 +41,11 @@ class Queue:
           print(curr.data,end=" ")
           curr=curr.next
   
+s=Queue()
+s.enqueue(10)
+s.enqueue(20)
+s.enqueue(30)
+s.dequue()
+s.enqueue(10)
+s.size()
+s.traverse()
